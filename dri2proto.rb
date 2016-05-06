5dc6357b4e096df038e9582c92cf68cbd72b4d37
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-06 02:37:27 +0000

    dri2proto: add 2.8 bottle for Linuxbrew.

--- dri2proto.rb
+++ dri2proto.rb
@@ -1,6 +1,11 @@
 # dri2proto: Build a bottle for Linuxbrew
 class Dri2proto < Formula
   desc "X.Org Protocol Headers: dri2proto"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "88361afd628af1a3efd25681666155629841f9174a1e376291770c56d658a802" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/proto/dri2proto-2.8.tar.bz2"
   sha256 "f9b55476def44fc7c459b2537d17dbc731e36ed5d416af7ca0b1e2e676f8aa04"
