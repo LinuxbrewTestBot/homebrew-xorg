76b6a89f4ef0a74e693509ee34e92de5a7cd8d94
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-06 02:41:14 +0000

    glproto: add 1.4.17 bottle for Linuxbrew.

--- glproto.rb
+++ glproto.rb
@@ -1,6 +1,11 @@
 # glproto: Build a bottle for Linuxbrew
 class Glproto < Formula
   desc "X.Org Protocol Headers: glproto"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ca1b0d27122bae1fd580bf9d46bfb7b925438c5e95ffb834c78bc9cb27d64904" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/proto/glproto-1.4.17.tar.bz2"
   sha256 "adaa94bded310a2bfcbb9deb4d751d965fcfe6fb3a2f6d242e2df2d6589dbe40"
