e6145f6d1bb8ab493f0971fc50d9a9cfc671e80b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-06 02:30:19 +0000

    compositeproto: add 0.4.2 bottle for Linuxbrew.

--- compositeproto.rb
+++ compositeproto.rb
@@ -1,6 +1,11 @@
 # compositeproto: Build a bottle for Linuxbrew
 class Compositeproto < Formula
   desc "X.Org Protocol Headers: compositeproto"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "4b1bf94e444d74a4acd8982fed297523b299373b72c775e98e008e7a9b5ba8c8" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/proto/compositeproto-0.4.2.tar.bz2"
   sha256 "049359f0be0b2b984a8149c966dd04e8c58e6eade2a4a309cf1126635ccd0cfc"
