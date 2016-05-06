172ba7153807558de4af2e8f430ff9bb80e4cae4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-06 12:41:06 +0000

    inputproto: add 2.3.1 bottle for Linuxbrew.

--- inputproto.rb
+++ inputproto.rb
@@ -1,6 +1,11 @@
 # inputproto: Build a bottle for Linuxbrew
 class Inputproto < Formula
   desc "X.Org Protocol Headers: inputproto"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c4ec367162b7b7fdaf076672426134b2b04da6b3c271723dba597ec81c01e6fd" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/proto/inputproto-2.3.1.tar.bz2"
   sha256 "5a47ee62053a6acef3a83f506312494be1461068d0b9269d818839703b95c1d1"
