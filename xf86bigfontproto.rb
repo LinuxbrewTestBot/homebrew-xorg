c4f05ba082c65f4cf1775630faef26552f983e4e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-07 02:00:05 +0000

    xf86bigfontproto: add 1.2.0 bottle for Linuxbrew.

--- xf86bigfontproto.rb
+++ xf86bigfontproto.rb
@@ -1,6 +1,11 @@
 # xf86bigfontproto: Build a bottle for Linuxbrew
 class Xf86bigfontproto < Formula
   desc "X.Org Protocol Headers: xf86bigfontproto"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "af0d2b1eedf5ed2a360b8c2b5dfa9d5eed671d178244e7a20fabb3f317cc437a" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/proto/xf86bigfontproto-1.2.0.tar.bz2"
   sha256 "ba9220e2c4475f5ed2ddaa7287426b30089e4d29bd58d35fad57ba5ea43e1648"
