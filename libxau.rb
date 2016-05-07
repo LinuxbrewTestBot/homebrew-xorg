a6dc00129cf8311e833b66c228d64a8810c5a5c9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-07 02:14:55 +0000

    libxau: add 1.0.8 bottle for Linuxbrew.

--- libxau.rb
+++ libxau.rb
@@ -1,6 +1,11 @@
 # libxau: Build a bottle for Linuxbrew
 class Libxau < Formula
   desc "X11 Authorization Protocol"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "d1582ba4746a6b2c326bf25abb627c80b1302d17ee08158eabf3f2e6205b2ec4" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/lib/libXau-1.0.8.tar.bz2"
   sha256 "fdd477320aeb5cdd67272838722d6b7d544887dfe7de46e1e7cc0c27c2bea4f2"
