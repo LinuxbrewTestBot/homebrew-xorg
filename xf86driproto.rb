bf641f6e8bb05d86e0da0f17c5991a00ac62b793
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-07 01:56:35 +0000

    xf86driproto: add 2.1.1 bottle for Linuxbrew.

--- xf86driproto.rb
+++ xf86driproto.rb
@@ -1,6 +1,11 @@
 # xf86driproto: Build a bottle for Linuxbrew
 class Xf86driproto < Formula
   desc "X.Org Protocol Headers: xf86driproto"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "add3e319e6aa797042f7b8eb8ca39b9213fdc642330769cd80f1656b007be6fa" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/proto/xf86driproto-2.1.1.tar.bz2"
   sha256 "9c4b8d7221cb6dc4309269ccc008a22753698ae9245a398a59df35f1404d661f"
