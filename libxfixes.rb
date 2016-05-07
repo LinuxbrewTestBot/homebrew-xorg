3968298a60f63fe7fa2b0f56015391700a1df5d1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-07 14:15:45 +0000

    libxfixes: add 5.0.1 bottle for Linuxbrew.

--- libxfixes.rb
+++ libxfixes.rb
@@ -1,6 +1,11 @@
 # libxfixes: Build a bottle for Linuxbrew
 class Libxfixes < Formula
   desc "X.Org Libraries: libXfixes"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "eff8e0a159e7b64ef860bace99d92bcfe6b72cf73229c302b3c0adbaab8c0eec" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/lib/libXfixes-5.0.1.tar.bz2"
   sha256 "63bec085084fa3caaee5180490dd871f1eb2020ba9e9b39a30f93693ffc34767"
