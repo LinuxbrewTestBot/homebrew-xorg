4f5c9f7a1602908ca50160b3dc6d9d14926e5e4b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-07 02:07:09 +0000

    xf86vidmodeproto: add 2.3.1 bottle for Linuxbrew.

--- xf86vidmodeproto.rb
+++ xf86vidmodeproto.rb
@@ -1,6 +1,11 @@
 # xf86vidmodeproto: Build a bottle for Linuxbrew
 class Xf86vidmodeproto < Formula
   desc "X.Org Protocol Headers: xf86vidmodeproto"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "fcf020ee7ae77f134702e0ae849e7d4211e7db404431ef0a02e6076c66e753cd" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/proto/xf86vidmodeproto-2.3.1.tar.bz2"
   sha256 "45d9499aa7b73203fd6b3505b0259624afed5c16b941bd04fcf123e5de698770"
