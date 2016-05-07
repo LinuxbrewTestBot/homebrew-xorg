5cbca44e1a0bd9fb63b7cb010806e7496d5dde6d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-07 01:49:32 +0000

    kbproto: add 1.0.7 bottle for Linuxbrew.

--- kbproto.rb
+++ kbproto.rb
@@ -1,6 +1,11 @@
 # kbproto: Build a bottle for Linuxbrew
 class Kbproto < Formula
   desc "X.Org Protocol Headers: kbproto"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "3d6b25f6545716ad960db544ddd46b60c5e73c46fcc4a8b6ebea34e8fcdaf478" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/proto/kbproto-1.0.7.tar.bz2"
   sha256 "f882210b76376e3fa006b11dbd890e56ec0942bc56e65d1249ff4af86f90b857"
