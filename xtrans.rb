0f279c3f971ac146e95f431eca693a39b12989e4
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-08 12:06:27 +0000

    xtrans: add 1.3.5 bottle for Linuxbrew.

--- xtrans.rb
+++ xtrans.rb
@@ -1,6 +1,11 @@
 # xtrans: Build a bottle for Linuxbrew
 class Xtrans < Formula
   desc "X.Org Libraries: xtrans"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "90e5852ee25ed85ba47acaa55723a0506c8313c599671b892024ac1766b15449" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/lib/xtrans-1.3.5.tar.bz2"
   sha256 "adbd3b36932ce4c062cd10f57d78a156ba98d618bdb6f50664da327502bc8301"
