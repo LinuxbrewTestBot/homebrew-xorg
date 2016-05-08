acb9fbc86fbfac8110ceb20aabbc778d5cd21f5a
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-08 13:41:34 +0000

    libxcursor: add 1.1.14 bottle for Linuxbrew.

--- libxcursor.rb
+++ libxcursor.rb
@@ -1,6 +1,10 @@
 # libxcursor: Build a bottle for Linuxbrew
 class Libxcursor < Formula
   desc "X.Org Libraries: libXcursor"
+  bottle do
+    sha256 "b416d2de1edef471cebc301b9865ae1ccd79480a6bae60685b637b2ca65f308a" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/lib/libXcursor-1.1.14.tar.bz2"
   sha256 "9bc6acb21ca14da51bda5bc912c8955bc6e5e433f0ab00c5e8bef842596c33df"
