33d6daed81f75d1e0eec3aa438d2c1854aeae5b9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-19 16:58:36 +0000

    libxshmfence: add 1.2 bottle for Linuxbrew.

--- libxshmfence.rb
+++ libxshmfence.rb
@@ -1,5 +1,10 @@
 class Libxshmfence < Formula
   desc "X.Org Libraries: libxshmfence"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5a9e167ece906a18ac6503d6b3f61c8b64f6b9749c1290079f31e8d9e712cf9d" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "http://ftp.x.org/pub/individual/lib/libxshmfence-1.2.tar.bz2"
   sha256 "d21b2d1fd78c1efbe1f2c16dae1cb23f8fd231dcf891465b8debe636a9054b0c"
