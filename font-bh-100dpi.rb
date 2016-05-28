2085a9178ca5acfcabba391c88fc8efb3ea2a1e1
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-28 02:25:38 +0000

    font-bh-100dpi: add 1.0.3 bottle for Linuxbrew.

--- font-bh-100dpi.rb
+++ font-bh-100dpi.rb
@@ -2,6 +2,10 @@
 class FontBh100dpi < Formula
   desc "X.Org Fonts: font bh 100dpi"
   homepage "http://www.x.org/"
+  bottle do
+    sha256 "f976d11d7993f6b520da1e5bba0d01f47e1d9ca696a9163dc7f4dbe297ea7f77" => :x86_64_linux
+  end
+
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7font.html
   url    "https://www.x.org/pub/individual/font/font-bh-100dpi-1.0.3.tar.bz2"
   mirror "http://xorg.freedesktop.org/archive/individual/font/font-bh-100dpi-1.0.3.tar.bz2"
