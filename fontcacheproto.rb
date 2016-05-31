d0ec40c1a9c94e31ba77a042a46e50243469ea8b
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-05-31 11:58:33 +0000

    fontcacheproto: add 0.1.3 bottle for Linuxbrew.

--- fontcacheproto.rb
+++ fontcacheproto.rb
@@ -2,6 +2,11 @@
 class Fontcacheproto < Formula
   desc "X.Org Proto: fontcacheproto"
   homepage "http://www.x.org/"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "9651b2ef34a2415b3c851959ec2a66d05c06aafad108bea465f996a9f8ad90c2" => :x86_64_linux
+  end
+
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url    "https://www.x.org/pub/individual/proto/fontcacheproto-0.1.3.tar.bz2"
   mirror "http://ftp.x.org/pub/individual/proto/fontcacheproto-0.1.3.tar.bz2"
