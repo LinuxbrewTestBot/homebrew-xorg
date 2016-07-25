3fa13d3e424af365e2db3767f5597ac1d5b5cc46
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-07-25 12:22:48 +0000

    xcursorgen: add 1.0.6 bottle for Linuxbrew.

--- xcursorgen.rb
+++ xcursorgen.rb
@@ -2,6 +2,11 @@
 class Xcursorgen < Formula
   desc "X.Org Applications: xcursorgen"
   homepage "http://www.x.org/"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "5eb5cc0e64c2c05bf5c8f626e0aa8f796bf8d1a572d1d6bff9916f6bae6a1510" => :x86_64_linux
+  end
+
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url    "https://www.x.org/pub/individual/app/xcursorgen-1.0.6.tar.bz2"
   mirror "http://ftp.x.org/pub/individual/app/xcursorgen-1.0.6.tar.bz2"
