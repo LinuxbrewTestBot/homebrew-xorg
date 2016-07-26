52b7555aa50cf64fd6611a4db396c7485892e474
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-07-26 12:07:52 +0000

    xmessage: add 1.0.4 bottle for Linuxbrew.

--- xmessage.rb
+++ xmessage.rb
@@ -2,6 +2,11 @@
 class Xmessage < Formula
   desc "X.Org Applications: xmessage"
   homepage "http://www.x.org/"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "6a38fe5a90818f80f322186e6372684893e3b7362007eee0b45015f07e295415" => :x86_64_linux
+  end
+
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7app.html
   url    "https://www.x.org/pub/individual/app/xmessage-1.0.4.tar.bz2"
   mirror "http://ftp.x.org/pub/individual/app/xmessage-1.0.4.tar.bz2"
