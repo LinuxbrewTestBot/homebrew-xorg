efd02d2476208f17aef11ad3e94ea2ff1cf1f54e
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-08-05 10:16:36 +0000

    font-bitstream-type1: add 1.0.3_1 bottle for Linuxbrew.

--- font-bitstream-type1.rb
+++ font-bitstream-type1.rb
@@ -2,6 +2,10 @@
 class FontBitstreamType1 < Formula
   desc "X.Org Fonts: font bitstream type1"
   homepage "http://www.x.org/"
+  bottle do
+    sha256 "0e5b156b1065f00a45d8be3828c439ec06583398639a9113b8173c73bee817fd" => :x86_64_linux
+  end
+
   ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7font.html
   url    "https://www.x.org/pub/individual/font/font-bitstream-type1-1.0.3.tar.bz2"
   mirror "https://xorg.freedesktop.org/archive/individual/font/font-bitstream-type1-1.0.3.tar.bz2"
