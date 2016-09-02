d9e5a01402ee203f48c7da5096b5aa8225a9ef3f
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-09-02 22:32:17 +0000

    libpthread-stubs: add 0.3 bottle for Linuxbrew.

--- libpthread-stubs.rb
+++ libpthread-stubs.rb
@@ -4,6 +4,11 @@ class LibpthreadStubs < Formula
   homepage "http://www.x.org/"
   url "https://xcb.freedesktop.org/dist/libpthread-stubs-0.3.tar.bz2"
   sha256 "35b6d54e3cc6f3ba28061da81af64b9a92b7b757319098172488a660e3d87299"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ab61c92ab2a15907df6a29d08963290c4896b1aa7939524932d343acbe7d8026" => :x86_64_linux
+  end
+
   # tag "linuxbrew"
 
   depends_on "pkg-config"  => :build
