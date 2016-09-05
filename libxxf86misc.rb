395788de26bba60feb76e75cb83f794d959a3cd2
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-09-05 13:37:58 +0000

    libxxf86misc: add 1.0.3 bottle for Linuxbrew.

--- libxxf86misc.rb
+++ libxxf86misc.rb
@@ -1,6 +1,11 @@
 # libxxf86misc: Build a bottle for Linuxbrew
 class Libxxf86misc < Formula
   desc "X.Org Libraries: libXxf86misc"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "b93c3e0e6f73b9e19682b33f58ab6ebe836fd3918a3c6b15ec4ff7bd588c39e3" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "https://www.x.org/archive//individual/lib/libXxf86misc-1.0.3.tar.gz"
   sha256 "358f692f793af00f6ef4c7a8566c1bcaeeea37e417337db3f519522cc1df3946"
