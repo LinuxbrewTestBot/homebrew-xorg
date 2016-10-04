11da6c85dc148e06e61bcbef2393e135c91b741d
LinuxbrewTestBot <testbot@linuxbrew.sh>
2016-10-04 14:38:36 +0000

    libva-intel-driver: add 1.7.2 bottle for Linuxbrew.

--- libva-intel-driver.rb
+++ libva-intel-driver.rb
@@ -4,6 +4,11 @@ class LibvaIntelDriver < Formula
   url "https://www.freedesktop.org/software/vaapi/releases/libva-intel-driver/libva-intel-driver-1.7.2.tar.bz2"
   sha256 "099e7bf6aa826971ec2caff235babf4da995f754e2ca16a73b258671130e67bc"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "ac66e934899c489a79a132c9f7cf85c3a8799e15248462ebbac72fd6330b982e" => :x86_64_linux
+  end
+
   # Build-time
   depends_on "pkg-config" => :build
 
