b9c19eb70e4dda29c4ea193510726d4f9581038c
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-01-15 19:36:29 +0000

    xf86miscproto: add 0.9.3 bottle for Linuxbrew.

--- xf86miscproto.rb
+++ xf86miscproto.rb
@@ -1,5 +1,10 @@
 class Xf86miscproto < Formula
   desc "X.Org Protocol Headers: xf86miscproto"
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "cfe8f123467f758fd21b99aba19a93e784f6656c9dc101e94840d300de4916de" => :x86_64_linux
+  end
+
   homepage "http://www.x.org/" ### http://www.linuxfromscratch.org/blfs/view/svn/x/x7lib.html
   url    "https://www.x.org/archive/individual/proto/xf86miscproto-0.9.3.tar.bz2"
   sha256 "45b8ec6a4a8ca21066dce117e09dcc88539862e616e60fb391de05b36f63b095"
