e5f620574c56af1f0958b5da9de73f6c1ff30319
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-09 04:26:53 +0000

    libxmu: update 1.1.2 bottle for Linuxbrew.

--- libxmu.rb
+++ libxmu.rb
@@ -6,8 +6,8 @@ class Libxmu < Formula
   # tag "linuxbrew"
 
   bottle do
-    rebuild 1
-    sha256 "2b0c683b9deddbed3c238bf167b96ada9e76587f22ed0a8d28d8f87955d38e20" => :x86_64_linux
+    rebuild 2
+    sha256 "805c8b57e202c862d4bed2573139d3d1c83ee105108c4fcd117c07f439d92c8d" => :x86_64_linux
   end
 
   option "without-test", "Skip compile-time tests"
