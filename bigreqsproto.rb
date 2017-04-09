e7659c03accf29c0e74b3ce1a77906a1b41f45b9
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-04-09 04:26:53 +0000

    bigreqsproto: update 1.1.2 bottle for Linuxbrew.

--- bigreqsproto.rb
+++ bigreqsproto.rb
@@ -6,8 +6,9 @@ class Bigreqsproto < Formula
   # tag "linuxbrew"
 
   bottle do
-    cellar :any
-    sha256 "204f49efe8ca9252a02645166810b3f1e8fe7d14e7298977cb41fc2bb3b945bd" => :x86_64_linux
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "07f7fafc18cc0a05b1b3b1561ea3b6ca0966abdcf74a4a99754cde7c44b2c7b5" => :x86_64_linux
   end
 
   option "with-specs", "Build specifications"
