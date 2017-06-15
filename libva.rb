23097580a3782d1392053135a0c908b73a3fdd11
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-15 03:05:09 +0000

    libva: update 1.7.3 bottle for Linuxbrew.

--- libva.rb
+++ libva.rb
@@ -5,7 +5,8 @@ class Libva < Formula
   sha256 "22bc139498065a7950d966dbdb000cad04905cbd3dc8f3541f80d36c4670b9d9"
 
   bottle do
-    sha256 "202cad95248823257d1d9cedefb76fc95593ef3dd61d93721a14f000f75bd0db" => :x86_64_linux
+    rebuild 1
+    sha256 "0bb8eb796323a1c17090e947151b0c647ec8cbe1484afae37265b5dcecd961fa" => :x86_64_linux
   end
 
   option "with-static", "Build static libraries (not recommended)"
