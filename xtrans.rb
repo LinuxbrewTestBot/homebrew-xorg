543892a281189b358e5ddc1f14511dabffb57d70
LinuxbrewTestBot <testbot@linuxbrew.sh>
2017-06-16 17:18:14 +0000

    xtrans: update 1.3.5 bottle for Linuxbrew.

--- xtrans.rb
+++ xtrans.rb
@@ -6,8 +6,9 @@ class Xtrans < Formula
   # tag "linuxbrew"
 
   bottle do
-    cellar :any
-    sha256 "90e5852ee25ed85ba47acaa55723a0506c8313c599671b892024ac1766b15449" => :x86_64_linux
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "a115d2aa55f7a903b6d6438f5d09fd29ecb7051062cf7ce4e1942fa931d68267" => :x86_64_linux
   end
 
   option "with-docs", "Build documentation"
